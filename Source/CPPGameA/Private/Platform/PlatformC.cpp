a4c8ab7b1543ade1a9b6efbdc0d74a01d2819341
Angel Gustavo <tavomx.987@gmail.com>
2025-01-19 19:02:29 -0600

    Platform

---
+++ Source/CPPGameA/Private/Platform/PlatformC.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Platform/PlatformC.h"
+
+// Sets default values
+APlatformC::APlatformC()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	PlatformMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Platform Mesh"));
+	PlatformMesh->SetupAttachment(GetRootComponent());
+
+}
+
+// Called when the game starts or when spawned
+void APlatformC::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlatformC::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
